f961b71bc09844c1aea303298acfed71fc5d793c
manalkiyani <manalkiyani687@gmail.com>
2025-02-10 10:28:43 +0500

    add corrected font path in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import "./globals.css";
 import localFont from "next/font/local"
 const ibmPlexSans = localFont({
   src: [
-    { path: '/fonts/IBMPlex-Sans-Regular.ttf', weight: '400', style: 'normal' },
-    { path: '/fonts/IBMPlex-Sans-Medium.ttf', weight: '500', style: 'normal' },
-    { path: '/fonts/IBMPlex-Sans-SemiBold.ttf', weight: '600', style: 'normal' },
-    { path: '/fonts/IBMPlex-Sans-Bold.ttf', weight: '700', style: 'normal' }
+    { path: '/fonts/IBMPlexSans-Regular.ttf', weight: '400', style: 'normal' },
+    { path: '/fonts/IBMPlexSans-Medium.ttf', weight: '500', style: 'normal' },
+    { path: '/fonts/IBMPlexSans-SemiBold.ttf', weight: '600', style: 'normal' },
+    { path: '/fonts/IBMPlexSans-Bold.ttf', weight: '700', style: 'normal' }
   ]
 });
 
